5a20b2256c81d0fb8319111854d4a3cd1baedef1
masankar666 <68776027+masankar666@users.noreply.github.com>
2020-07-29 21:16:42 +0530

    Add Two Number

---
+++ Add Two Number.py
@@ -0,0 +1,11 @@
+print ('hello, world!')
+
+
+#Add Two Number
+print ('Adding Two Number')
+print('Enter the First umber:')
+print('Enter the second number:')
+num1=5
+num2=6.7
+sum=num1+num2
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
