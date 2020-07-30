ac30e11c927e3a5f4759faca6be32a25a0c79365
masankar666 <68776027+masankar666@users.noreply.github.com>
2020-07-30 23:19:47 +0530

    swap
    swapping done without using temporary

---
+++ swap_not_temp.py
@@ -0,0 +1,23 @@
+x = 5
+y = 10
+
+x, y = y, x
+print("x =", x)
+print("y =", y)
+
+#Done
+#Addition and Subtraction
+#x = x + y
+#y = x - y
+#x = x - y
+
+#Multiplication and Division
+
+#x = x * y
+#y = x / y
+#x = x / y
+
+#XOR swap   --> This algorithm works for integers only
+#x = x ^ y
+#y = x ^ y
+#x = x ^ y
