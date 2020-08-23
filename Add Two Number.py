f4dec2e74228e342086e3c6a0a070b6359fba5a2
masankar666 <68776027+masankar666@users.noreply.github.com>
2020-08-23 19:58:20 +0530

    User input model
    Dynamic input

--- Add Two Number.py
+++ Add Two Number.py
@@ -6,3 +6,15 @@ num1=5
 num2=6.7
 sum=num1+num2
 print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
+
+# Program to add two numbers entered by the user
+
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print(num1, "+", num2, "=", sum)
