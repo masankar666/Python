1ee8c96ac1b4c4115e09343f7257686571727195
masankar666 <68776027+masankar666@users.noreply.github.com>
2020-08-23 20:05:52 +0530

    Square_root.py
    Square root findings

---
+++ squareroot.py
@@ -0,0 +1,19 @@
+# Change this value and test yourself
+num = 6.25
+
+# ** is used for exponentiation
+num_sqrt = num ** 0.5
+print('The square root of', num, 'is:', num_sqrt )
+
+
+
+
+# Find the square root of real or complex numbers
+# Import the complex math module
+
+import cmath
+
+num = 64-64j
+
+num_sqrt = cmath.sqrt(num)
+print('The square root of', num, 'is', num_sqrt)
